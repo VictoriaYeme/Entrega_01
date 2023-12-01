138894a899494661bc6684c1e9daa2d117a22b7e
VictoriaYeme <mvyeme@uc.cl>
2023-12-01 13:05:17 -0300

    guardar

--- Entrega_05/docs/script.js
+++ Entrega_05/docs/script.js
@@ -1,7 +1,7 @@
 function mostrarInfo(id) {
-    // Ocultar todas las secciones
-    var secciones = document.querySelectorAll('section');
-    secciones.forEach(function (seccion) {
+    // Ocultar todas las secciones de información específica
+    var seccionesInfo = document.querySelectorAll('.info-box');
+    seccionesInfo.forEach(function (seccion) {
         seccion.style.display = 'none';
     });
 
@@ -9,5 +9,16 @@ function mostrarInfo(id) {
     var seccionMostrar = document.getElementById(id);
     if (seccionMostrar) {
         seccionMostrar.style.display = 'block';
+
+        // Mostrar el cuadro adicional en el Menú Principal
+        var cuadroAdicional = document.getElementById('additional-menu-box');
+        if (id === 'info1') {
+            cuadroAdicional.style.display = 'block';
+        } else {
+            cuadroAdicional.style.display = 'none';
+        }
     }
 }
+
+// Asegúrate de llamar a la función al cargar la página para mostrar la sección inicial
+mostrarInfo('info1');
