1e28918e07b71b3c2c2b68cd6aa36905fe8d4aae
FTGallet <ftgallet@uc.cl>
2023-12-14 17:56:49 -0300

    barra progreso

--- docs/script.js
+++ docs/script.js
@@ -20,6 +20,8 @@ function mostrarInfo(id) {
     }
 }
 
+
+
 function mostrarInformacionAdicional(region) {
     // Obtener la sección de información adicional correspondiente a la región
     var regionInfo = document.getElementById(region + '-info');
@@ -36,5 +38,38 @@ function mostrarInformacionAdicional(region) {
     }
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+    const progressBar = document.querySelector('.progress-bar');
+    const arrow = document.querySelector('.arrow');
+    const checkboxes = document.querySelectorAll('.questions input[type="checkbox"]');
+  
+    // Mueve la flecha según el progreso al cargar la página
+    updateProgressBar();
+  
+    // Agrega un evento clic a cada checkbox para manejar las respuestas
+    checkboxes.forEach((checkbox) => {
+      checkbox.addEventListener('change', updateProgressBar);
+    });
+  
+    function updateProgressBar() {
+      // Calcula el número total de respuestas "sí"
+      const respuestasPositivas = Array.from(checkboxes).filter((checkbox) => checkbox.checked).length;
+  
+      // Cada "sí" contribuye con un 10%
+      const porcentajeRespuestasPositivas = respuestasPositivas * 10;
+  
+      // Limita el porcentaje al 100% para evitar que la flecha se desplace más allá del final de la barra
+      const porcentajeFinal = Math.min(porcentajeRespuestasPositivas, 100);
+  
+      // Mueve la flecha según el progreso
+      arrow.style.transform = `translateX(${porcentajeFinal}%)`;
+    }
+   
+  
+  
+
+
 // Asegúrate de llamar a la función al cargar la página para mostrar la sección inicial
 mostrarInfo('info1');
+
+});
